803a880ae2bf0f0cf863279fce90890b3f313466
devLucasAquino <joaolucas.aqalves111@outlook.com>
2024-08-03 18:16:06 -0300

    feat: implementando redux toolkit

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,8 @@ import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 import { Provider } from 'react-redux'
-import { store } from "../redux-old.ts"
+// import { store } from "../redux-old.ts"
+import { store } from './store.ts'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
